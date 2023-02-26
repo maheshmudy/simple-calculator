5d2d78e2c5d43c27ee89184933f9a2e9ee93c35b
maheshmudy <maheshplamudy7@gmail.com>
2023-02-26 14:08:36 +0530

    css styles and designs added

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-function bclick (val)
-        {
-            document.getElementById("display").value+=val;
-        }
-        function displayClear()
-        {
-            document.getElementById("display").value="";
-        }
-        function eclick()
-        {
-            var text=document.getElementById("display").value ;
-            var result=eval(text);
-            document.getElementById("display").value=result;
-        }
+function buttonClick(val) {
+    document.getElementById("display").value += val;
+}
+function displayClear() {
+    document.getElementById("display").value = "";
+}
+function equalClick() {
+    var text = document.getElementById("display").value;
+    var result = eval(text);
+    document.getElementById("display").value = result;
+}
